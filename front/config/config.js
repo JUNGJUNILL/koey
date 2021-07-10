d8d19b2bf489214eedf916eb1c7356a20f7e4ee6
dala1207@naver.com <dala1207@naver.com>
2021-07-10 16:46:30 +0900

    commit_20210710

--- front/config/config.js
+++ front/config/config.js
@@ -2,5 +2,8 @@
 //export const backImageUrl =process.env.NODE_ENV === 'production' ? '백엔드 도메인 주소' : 'http://localhost:3095';
 //export const backUrl = process.env.NODE_ENV === 'production' ? '백엔드 도메인 주소' : 'http://localhost:3095/api';
 
-export const backImageUrl ='http://captainryan.iptime.org:3095';
-export const backUrl = 'http://captainryan.iptime.org:3095/api';
+// export const backImageUrl ='http://captainryan.iptime.org:3095';
+// export const backUrl = 'http://captainryan.iptime.org:3095/api';
+
+export const backImageUrl ='http://localhost:3095';
+export const backUrl = 'http://localhost:3095/api';
