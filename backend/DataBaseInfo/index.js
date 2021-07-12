467816dc24f75b165729a53a8cfdb409f29af9b3
dala1207@naver.com <dala1207@naver.com>
2021-07-12 10:12:47 +0900

    commit_20210712

--- backend/DataBaseInfo/index.js
+++ backend/DataBaseInfo/index.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb'); 
 const pool = mariadb.createPool({
     database:'mariadb',
-    host:'localhost',
+    host:'captainryan.iptime.org',
     user:'root',
     password:'6942', 
     port:3307, 
