4e3d4366431fb237fb83c4439b5c5f2e1de3abbf
dala1207@naver.com <dala1207@naver.com>
2021-07-13 19:39:25 +0900

    commit

--- front/config/config.js
+++ front/config/config.js
@@ -2,8 +2,8 @@
 //export const backImageUrl =process.env.NODE_ENV === 'production' ? '백엔드 도메인 주소' : 'http://localhost:3095';
 //export const backUrl = process.env.NODE_ENV === 'production' ? '백엔드 도메인 주소' : 'http://localhost:3095/api';
 
-export const backImageUrl ='http://3.35.0.24:8080';
-export const backUrl = 'http://3.35.0.24:8080/api';
+export const backImageUrl ='http://3.35.0.24:3333';
+export const backUrl = 'http://3.35.0.24:3333/api';
 
 // export const backImageUrl ='http://localhost:3095';
 // export const backUrl = 'http://localhost:3095/api';
