fa8d05e3b0d24396f3e0b2fb5bdb351dc37c4d55
dala1207@naver.com <dala1207@naver.com>
2021-07-19 19:49:38 +0900

    commit

--- front/pages/posts/mainPosts_1001.js
+++ front/pages/posts/mainPosts_1001.js
@@ -136,7 +136,11 @@ const mainPosts_1001 = ()=>{
                     이미지 리사이징 (무슨 이유에서인지 잘 안됨 일단 원본으로 가자) 
                     <img src={v.imageCount > 0 ? `${backUrl}/imgResizing?size=80x60&posf=${posf}&fileName=${encodeURIComponent(v.firstImageName)}` :`${backImageUrl}/noimages.gif`} />
                    */}
-                  {/*이미지 원본 */}
+                  {/*이미지 원본
+                    <img src={v.imageCount > 0 ? `${backImageUrl}/${posf}/${v.firstImageName}` :`${backImageUrl}/noimages.gif`} />
+                  */}
+
+                  {/*AWS용*/}
                   <img src={v.imageCount > 0 ? `${backImageUrl}/${posf}/${v.firstImageName}` :`${backImageUrl}/noimages.gif`} />
 
 
