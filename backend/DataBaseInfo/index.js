cf3f7dba1b92cf9ead807d34eaa188143af360e9
dala1207@naver.com <dala1207@naver.com>
2021-07-22 15:12:07 +0900

    commit

--- backend/DataBaseInfo/index.js
+++ backend/DataBaseInfo/index.js
@@ -1,9 +1,12 @@
 const mariadb = require('mariadb'); 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const pool = mariadb.createPool({
-    database:'mariadb',
-    host:'captainryan.iptime.org',
-    user:'root',
-    password:'6942', 
-    port:3307, 
+    database:process.env.NODE_ENV === 'production'? process.env.DBNAME:process.env.DBNAME_LOCAL,
+    host:process.env.NODE_ENV === 'production'? process.env.HOST:process.env.HOST_LOCAL,
+    user:process.env.NODE_ENV === 'production'? process.env.USER:process.env.USER_LOCAL,
+    password:process.env.NODE_ENV === 'production'? process.env.PASSWORD:process.env.PASSWORD_LOCAL, 
+    port:process.env.NODE_ENV === 'production'? process.env.PORT:process.env.PORT_LOCAL, 
 }); 
 module.exports =  pool; 
