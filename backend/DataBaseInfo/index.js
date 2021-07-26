61e3ebc23714cc1b3dbb84077d1d655c4789d31b
dala1207@naver.com <dala1207@naver.com>
2021-07-26 14:53:41 +0900

    commit

--- backend/DataBaseInfo/index.js
+++ backend/DataBaseInfo/index.js
@@ -1,11 +1,14 @@
 const mariadb = require('mariadb'); 
-const dotenv = require('dotenv');
-dotenv.config();
+//const dotenv = require('dotenv');
+const path = require('path'); 
+require('dotenv').config({ path: path.join(__dirname, '../','.env') });
+
+
 
 const pool = mariadb.createPool({
     database:process.env.NODE_ENV === 'production'? process.env.DBNAME:process.env.DBNAME_LOCAL,
     host:process.env.NODE_ENV === 'production'? process.env.HOST:process.env.HOST_LOCAL,
-    user:process.env.NODE_ENV === 'production'? process.env.USER:process.env.USER_LOCAL,
+    user:process.env.NODE_ENV === 'production'? process.env.AUSER:process.env.USER_LOCAL,
     password:process.env.NODE_ENV === 'production'? process.env.PASSWORD:process.env.PASSWORD_LOCAL, 
     port:process.env.NODE_ENV === 'production'? process.env.PORT:process.env.PORT_LOCAL, 
 }); 
