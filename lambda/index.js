ddf91025687799fef00507238433cadac7658bcb
dala1207@naver.com <dala1207@naver.com>
2021-07-29 11:16:53 +0900

    commit

--- lambda/index.js
+++ lambda/index.js
@@ -1,11 +1,12 @@
-const AWS = require('aws-sdk');
-const sharp = require('sharp');
+const faker = require('faker');
 
-const s3 = new AWS.S3();
+const hello = faker.lorem.paragraph(); 
 
-exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
-  const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/12312312_abc.png
-  console.log(Bucket, Key);
- 
-}
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body:hello,
+    };
+    return response;
+};
