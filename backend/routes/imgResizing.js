61119c811d654d661425163e97256a2ba4062e70
dala1207@naver.com <dala1207@naver.com>
2021-07-29 19:04:01 +0900

    commit

--- backend/routes/imgResizing.js
+++ backend/routes/imgResizing.js
@@ -3,6 +3,7 @@ const sharp = require('sharp');
 const router = express.Router();
 const path   =require('path');
 const fs = require('fs'); 
+const got = require('got'); 
 
 
 
@@ -14,12 +15,17 @@ router.get('/' ,async (req,res,next)=>{
     let {size , posf, fileName} = req.query;
     const imageDir = path.join(__dirname,'../images',posf); 
     const filename =  path.join(imageDir,decodeURIComponent(fileName)); 
+    console.log('filename==>',filename);
 
+    //외부 이미지를 resize 할 경우
+    const buf=await got('https://upload.wikimedia.org/wikipedia/ko/6/60/%EA%B8%B0%EC%83%9D%EC%B6%A9_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg')
+                    .buffer();
+    //D:\git Repository\koey\backend\images\1001\1일_1625652527838_계란빵.jpg
     if(size) {
         size = size.split('x');
         return res.status(200).end(
             
-            await sharp(filename).resize(
+            await sharp(buf).resize(
                 {width:parseInt(size[0]), 
                 height:parseInt(size[1]), 
                 position:'top',
