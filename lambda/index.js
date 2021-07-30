427105535f7432572e78caac43792721763d0106
dala1207@naver.com <dala1207@naver.com>
2021-07-30 15:27:00 +0900

    commit

--- lambda/index.js
+++ lambda/index.js
@@ -1,39 +1,10 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
-
-const got = require('got'); 
-const sharp = require('sharp');
-const path   =require('path');
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/imageResize-lambda', (req, res) => {
-
-  const {url,size}=req.query; 
-  const bufferImage=await got(url).buffer();
-  const imageDir = path.join(__dirname,'../backend','../images');
-  const filename =  path.join(imageDir,'noimages.gif'); 
-  console.log('imageDir==>',imageDir,'filename==>',filename); 
-
-  if(size) {
-    size = size.split('x');
-    return res.status(200).end(
-        
-        await sharp(bufferImage).resize(
-            {width:parseInt(size[0]), 
-            height:parseInt(size[1]), 
-            position:'top',
-
-        }).toBuffer());
-
-  } else {
-
-      return  res.status(200).end(fs.readFileSync(filename));
-
-  }
-
-
+app.get('/helloWorld/imageResize-lambda', (req, res) => {
+  res.send('hello express');
 });
 
 //app.listen(3000, () => console.log(`Listening on: 3000`));
