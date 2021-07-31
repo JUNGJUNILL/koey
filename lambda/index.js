937a5a89020ea4b12cd30d64b879673f1c192c0a
dala1207@naver.com <dala1207@naver.com>
2021-07-31 14:48:59 +0900

    commit

--- lambda/index.js
+++ lambda/index.js
@@ -10,29 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get('/helloWorld/imageResize-lambda', async (req, res) => {
 
-  let {url,size}=req.query; 
-  const array = hello.split('.'); 
-  const ext = array[array.length-1]; 
-  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
-  const bufferImage=await got('https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1600,c_limit/phonepicutres-TA.jpg').buffer();
 
-  if(size) {
-    size = size.split('x');   
-    res.end(`hello express!!!!,${size[0]},${size[1]}`);
-    //  res.end(
-        
-    //     await sharp(bufferImage).resize(
-    //         {width:parseInt(size[0]), 
-    //         height:parseInt(size[1]), 
-    //         position:'top',
-
-    //     }).toFormat(requiredFormat).toBuffer());
-
-  } else {
-
-    res.end('hello express!!!!');
-
-  }
+   res.end('hello express!!!!');
 
 
 });
