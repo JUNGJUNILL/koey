e798b749ccb8469b49f8d2d48e7b2c543bd5efff
JUNGJUNILL <dala1207@naver.com>
2022-08-10 16:43:15 +0900

    commit

--- front/components/Header/HeaderComponenet.js
+++ front/components/Header/HeaderComponenet.js
@@ -1,6 +1,7 @@
 
 import jsMetaImage from '../../public/jsMetaImage.gif';
 import { backImageUrl,AWSImageUrl } from '../../config/config';
+import Head from "next/head";
 
 const HeaderComponenet =({posf,title,image})=>{
 
@@ -16,12 +17,12 @@ const HeaderComponenet =({posf,title,image})=>{
     }
 
     return(
-        <head>
+        <Head>
                 <meta property="og:url" content="http://www.jscompany.live"></meta>
                 <meta property="og:image" content={rootUrl.length===0?jsMetaImage:rootUrl}></meta>
                 <meta property="og:title" content={title} />
                 <meta property="og:description" content='중소기업 썰 ㅋ'></meta>
-        </head>
+        </Head>
     )
 
 }
