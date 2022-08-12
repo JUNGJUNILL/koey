ed0607a40ccd08a496a90915bbca58c1f2ed161b
JUNGJUNILL <dala1207@naver.com>
2022-08-12 15:47:15 +0900

    commit

--- front/components/Header/HeaderComponenet.js
+++ front/components/Header/HeaderComponenet.js
@@ -1,12 +1,7 @@
 
 import jsMetaImage from '../../public/jsMetaImage.gif';
 import { backImageUrl,AWSImageUrl } from '../../config/config';
-import Head from "next/head";
-import wrapper from '../../store/configureStore';
 import { Helmet } from "react-helmet"
-
-import {END} from 'redux-saga'; 
-import wrapper from '../../store/configureStore';
 
 
 const HeaderComponenet =({posf,title,image,contents})=>{
