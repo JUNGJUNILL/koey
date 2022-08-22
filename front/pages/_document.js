56ce8c8136e8d7c04c52c05115bc0028c5a488ac
JUNGJUNILL <dala1207@naver.com>
2022-08-22 13:09:06 +0900

    commit

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -12,7 +12,8 @@ class MyDocument extends Document {
         <Head> 
         <meta charSet="utf-8" />
         <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
-         {/*구글에드 센스_20210902 */}
+         {/*구글에드 센스_20210902 
+
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"/>
         <script
         dangerouslySetInnerHTML={{
@@ -23,7 +24,7 @@ class MyDocument extends Document {
             });
               `
         }}/>
-
+      */}
     
         </Head>
         <body>
