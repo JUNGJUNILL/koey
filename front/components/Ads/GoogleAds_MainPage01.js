d79b005b7eb6c787f09b8fca159175fc1a9f3d9e
JUNGJUNILL <dala1207@naver.com>
2022-08-31 13:03:05 +0900

    commit

--- front/components/Ads/GoogleAds_MainPage01.js
+++ front/components/Ads/GoogleAds_MainPage01.js
@@ -11,14 +11,12 @@ const GoogleAds_MainPage01 =()=>{
 
 
     return (
-        <div style={{border:'1px solid'}}>
         <ins className="adsbygoogle"
             style={{display:'block', marginTop:'3%',marginBottom:'3px',textAlign:'center'}}
             data-ad-layout={"in-article"}
             data-ad-format={"fluid"}
             data-ad-client={"ca-pub-9160341796142118"}
             data-ad-slot={"9727724047"}></ins>
-        </div>
 )
 
 }
