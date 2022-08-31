9a11f203cbe152f5837fe5c1776ac8cf12021b6b
JUNGJUNILL <dala1207@naver.com>
2022-08-31 10:32:49 +0900

    commit

--- front/components/Ads/GooleAds_DetailPaage.js
+++ front/components/Ads/GooleAds_DetailPaage.js
@@ -9,16 +9,13 @@ const GoogleAds_DetailPage = () =>{
     },[])
 
     return (
-            <div style={{border:'1px solid'}}>
 
-         
             <ins className="adsbygoogle"
                 style={{display:'block', textAlign:'center'}}
                 data-ad-layout={"in-article"}
                 data-ad-format={"fluid"}
                 data-ad-client={"ca-pub-9160341796142118"}
                 data-ad-slot={"6162979590"}></ins>
-            </div>
     )
 
 }
