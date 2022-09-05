47b31d341e50669c807f41c52f6899dabd63dd29
JUNGJUNILL <dala1207@naver.com>
2022-09-05 13:48:20 +0900

    commit

--- front/components/mainPosts_1001/ImageUploadComponent.js
+++ front/components/mainPosts_1001/ImageUploadComponent.js
@@ -28,7 +28,7 @@ const ImageUploadComponent = ({postFlag,updateFlag,imageFileName,removeImage,rem
                                                             //`${backUrl}/imgResizing?size=60x60&posf=${posf}&fileName=${backImageUrl}/${posf}/${v}`} /> 
                                                             `${backImageUrl}/${posf}/${v.src}`  } />
                 <br/>
-                <Button style={{width:'60px'}}><CloseOutlined onClick={()=>removeImage(v.src,posf,v.imageId,v.postId,v.userId,v.submitDay,v.update)} /></Button>
+                <Button style={{width:'60px'}}><CloseOutlined onClick={()=>removeImage(v.src.split('/')[v.src.split('/').length-1,posf,v.imageId,v.postId,v.userId,v.submitDay,v.update)} /></Button>
             </div>
             ))}
 
