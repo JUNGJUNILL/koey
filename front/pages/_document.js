12470b47a2d057b1e37c5b7df735ef72b71a042a
JUNGJUNILL <dala1207@naver.com>
2022-09-22 09:10:19 +0900

    commit

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -11,6 +11,9 @@ class MyDocument extends Document {
       <Html>
         <Head> 
         <meta charSet="utf-8" />
+        <meta name="robots" content='index,follow' />
+        <meta name ="author" content="JJI" />
+        <meta name ="reply-to" content="devjji1207@gmail.com" />
         <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
          {/*구글에드 센스_20210902*/}
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"/>
