e4fca403118ec7feff6559850a33346c171e5f45
JUNGJUNILL <dala1207@naver.com>
2022-11-12 21:58:36 +0900

    commit

--- front/components/Ads/GooleAds_footer.js
+++ front/components/Ads/GooleAds_footer.js
@@ -3,9 +3,15 @@ import React , {useEffect}from 'react'
 
 const GooleAds_footer =() =>{
 
+    useEffect(()=>{
+        if(window) (window.adsbygoogle = window.adsbygoogle || []).push({});
+
+    },[])
+
     return (
         <>
             <ins className="adsbygoogle"
+            id="GooleAds_footer"
             style={{display:'block',minWidth:'360px',maxWidth:'970px',width:'100%',height:'90px' ,border:'solid 1px'}}
             data-ad-client={"ca-pub-9160341796142118"}
             data-ad-slot={"1782797173"}
--- front/components/Ads/GooleAds_header.js
+++ front/components/Ads/GooleAds_header.js
@@ -9,6 +9,7 @@ const GooleAds_header =() =>{
     return (
         <>
             <ins className="adsbygoogle"
+            id="GooleAds_header"
             style={{display:'block',minWidth:'360px',maxWidth:'970px',width:'100%',height:'90px'}}
             data-ad-client={"ca-pub-9160341796142118"}
             data-ad-slot={"1799109040"}
