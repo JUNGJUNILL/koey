279ab1fee569bee4aab1ddb8e5c4f8c5947daed0
JUNGJUNILL <dala1207@naver.com>
2022-11-13 01:28:31 +0900

    commit

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -115,7 +115,7 @@ const AppLayOut = ({children}) =>{
            {/*  <Link href={{pathname:'/tools/arrearsofwages'}}><a>임금채불 사업장</a></Link> */}
         </nav>
         {/*구글광고*/}
-        {mainPosts_1001 && <GooleAds_header />}
+        {(mainPosts_1001 && mainPosts_1001.length>0) && <GooleAds_header />}
                 
 
     {children}
