362b8a840dba3d31dec693da2851453ea78d4723
JUNGJUNILL <dala1207@naver.com>
2022-11-13 01:22:08 +0900

    commit

--- front/components/Ads/GooleAds_header.js
+++ front/components/Ads/GooleAds_header.js
@@ -11,7 +11,7 @@ const GooleAds_header =() =>{
     return (
         <p>
             <ins className="adsbygoogle"
-            style={{display:'block',minWidth:'360px',maxWidth:'970px',width:'100%',height:'90px',border:'solid 1px'}}
+            style={{display:'block',minWidth:'360px',maxWidth:'970px',width:'100%',height:'90px'}}
             data-ad-client={"ca-pub-9160341796142118"}
             data-ad-slot={"1799109040"}
             ></ins>
